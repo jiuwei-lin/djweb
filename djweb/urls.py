b1e2b0e4dbaf424287454fe821e86cc93761c956
neo@kfsyscc.org <neo@kfsyscc.org>
2019-06-23 09:40:57 +0800

    + Templagte & View

--- djweb/urls.py
+++ djweb/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mblog.views import homepage, showpost
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mblog/', homepage),
+    path('mblog/post/<slug:slug>/', showpost),
+
 ]
--- mblog/views.py
+++ mblog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
+
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    now = datetime.now()
+    return render(request, 'mblog.html', locals())
+
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'mblog_post.html', locals())
+    except:
+        return redirect('/')
+
+
+""" 
+Samle: HttpResponse
+
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+        post_lists.append(
+            "<small>{}</small><br/><br/>".format(str(post.body)))
+
+    return HttpResponse(post_lists)
+"""
